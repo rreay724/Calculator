e070627ccef73dbc731cb6a5101c127f435d26ce
rreay724 <rreay724@gmail.com>
2018-08-24 17:41:13 -0400

    Updated color and style

--- app/src/main/java/com/rreay/calculator/MainActivity.java
+++ app/src/main/java/com/rreay/calculator/MainActivity.java
@@ -2,11 +2,11 @@ package com.rreay.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
